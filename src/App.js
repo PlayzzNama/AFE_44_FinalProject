68673e3dab2b5d95b3ccdc70f922d39c588a30a7
Awen <69popovichivan69@gmail.com>
2024-11-09 17:52:56 +0200

    Added more new Components Added Components: Cryptocurrencies.jsx CryptoDetails.jsx Exchanges.jsx Homepage.jsx News.jsx
    Also added api:
    cryptoApi.js
    
    Also added some tech-files:
    store.js
    
    And modified:
    index.js (in Components);
    Navbar.jsx
    
    Added footer in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
-import { Layout, Typography, Space } from 'antd';
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom"; // Updated from Switch to Routes
+import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from './Components';
+import {
+  Navbar,
+  Exchanges,
+  Homepage,
+  Cryptocurrencies,
+  News,
+  CryptoDetails,
+} from "./Components";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className='app'>
-        <div className='navbar'>
-            <Navbar/>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              {" "}
+              {/* Updated from Switch to Routes */}
+              <Route exact path="/" element={<Homepage />} />
+              <Route exact path="/exchanges" element={<Exchanges />} />
+              <Route
+                exact
+                path="/crypto-currencies"
+                element={<Cryptocurrencies />}
+              />
+              <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+              <Route exact path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            CoinOrbit <br />
+            All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
         </div>
-        <div className='main'>
-
-        </div>
-        <div className='footer'>
-
-        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
