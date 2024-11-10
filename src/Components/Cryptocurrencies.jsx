0662318cccb0cedb5caa93bd5dfccdbd2e5218a2
Awen <69popovichivan69@gmail.com>
2024-11-10 23:55:55 +0200

    Pre-last commit
    1. Finished all components expect About-me page

--- src/Components/Cryptocurrencies.jsx
+++ src/Components/Cryptocurrencies.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = () => {
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    if (cryptosList?.data?.coins) {
+      console.log('Cryptos List:', cryptosList.data.coins); // Log the data to verify structure
+      setCryptos(cryptosList.data.coins);
+
+      const filteredData = cryptosList.data.coins.filter((coin) =>
+        coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+
+      setCryptos(filteredData);
+    }
+  }, [cryptosList, searchTerm]);
+
+  if (isFetching) return "Loading...";
+
   return (
-    <div>Cryptocurrencies</div>
-  )
-}
+    <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
 
-export default Cryptocurrencies
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((currency) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.uuid}>
+            <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
+              <Card
+                title={`${currency.rank}. ${currency.name}`}
+                extra={
+                  <img
+                    alt="crypto-image"
+                    className="crypto-image"
+                    src={currency.iconUrl}
+                  />
+                }
+                hoverable
+              >
+                <p>Price: {millify(currency.price)}</p>
+                <p>Market Cap: {millify(currency.marketCap)}</p>
+                <p>Daily Change: {millify(currency.change)}%</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Cryptocurrencies;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+import { HomeOutlined, MoneyCollectOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../assets/logo.png'
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
+
+  useEffect (() => {
+      if (screenSize < 801) {
+        setActiveMenu(false)
+      } else {
+        setActiveMenu(true)
+      }
+  }, [screenSize]) 
+
   return (
     <div className='nav-container'>
         <div className='logo-container'>
@@ -12,7 +33,11 @@ const Navbar = () => {
             <Typography.Title level={2} className='logo'>
                 <Link to="/">CoinOrbit</Link>
             </Typography.Title>
+            <Button className='menu-control-container' onClick={() => setActiveMenu(!activeMenu)}>
+              <MenuOutlined />
+            </Button>
         </div>
+        {activeMenu && (
         <Menu theme='dark'>
           <Menu.Item icon={<HomeOutlined/>}>
             <Link to="/">Home</Link>
@@ -21,12 +46,10 @@ const Navbar = () => {
             <Link to="/cryptocurrencies">Cryptocurrencies</Link>
           </Menu.Item>
           <Menu.Item icon={<MoneyCollectOutlined/>}>
-            <Link to="/exchanges">Exchanges</Link>
-          </Menu.Item>
-          <Menu.Item icon={<BulbOutlined/>}>
-            <Link to="/news">News</Link>
+            <Link to="/about-me">About me</Link>
           </Menu.Item>
         </Menu>
+        )}
     </div>
   )
 }
